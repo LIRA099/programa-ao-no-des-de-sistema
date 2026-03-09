f210445cdc0cd2916aacc858dedd512a39184f21
LIRA099 <felipe.rodrigues.lira@escola.pr.gov.br>
2026-03-09 11:14:39 -0300

    Add files via upload

--- aula 1 instrucao atribuicao.py
+++ aula 1 instrucao atribuicao.py
@@ -1,5 +1,11 @@
 #variavel e um espaço de memoria que tem um nome e guarda algum dado do meu programa
 numero = 1
 resultado = (numero + numero) * 5
-print(resultado)
+
+#expressoes sao quaisquer combinacoes de
+#valor, variavel e operadores
+resultado = numero + numero + 5
+resultado = (numero * (numero * numero) + 33)
+
+print(numero)
 
